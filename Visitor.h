b6f7cc12810de34c1552cfb905aa16b055bc65b7
mohamedabdelmohsen16010245 <mohamedmizo392006@gmail.com>
2025-11-12 15:24:55 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,53 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#infndef animal.h
+#define animal.h
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define visitor class here
+class animal{
+  
+  private :
+  
+  string name ;
+  int age ;
+  bool ishungry;
+  
+  public :
+   animal (string name , int age , bool ishungry ){
+     
+     this->name=name;
+     this->age=age;
+     this->ishungry=ishungry;
+   }
+  virtual void display(){
+    
+    cout<<"name"<<name<<endl;
+    cout<<"age"<<age<<endl;
+    cout<<"ishungry"<<ishungry<<endl;
+  }
+  
+  virtualvoid feed(){
+    if (animal ishungry){
+      cout<<"animal is hungry"<<endl;
+       return feed ;
 
-
-
-
-
-
-
-#endif
+    }
+    else {
+      cout<<"animal is not hungry"<<endl;
+      return dont feed ;
+    }
+  }
+  
+  subclass mammal{
+    string FurColor
+    
+  }
+  subclass bird{
+    float wingSpan;
+    
+  }
+  subclass reptile{
+    bool isVenomous;
+  }
+#endif  
+};
