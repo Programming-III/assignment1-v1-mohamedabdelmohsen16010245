2214d0578369ae0624f217f3eccb317992e7457b
mohamedabdelmohsen16010245 <mohamedmizo392006@gmail.com>
2025-11-12 15:26:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
 #include <iostream>
 using namespace std;
 
-
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
+int main() 
+{
+  
+  
+  Enclosure(name,age,isVenomous,ishungry){
+  Enclosure(lion,5,hungry);
+  Enclosure(parrot,2, not hungry);
+  Enclosure(snake,3,Venomous,hungry);
+  }
+  
+  
+vistor(vistorName,ticketsBought){
+  vistor(Sara Ali,3);
+}
+  
+  animal*=animal
+  
+    cout << "Hello, World!";
     return 0;
 }
